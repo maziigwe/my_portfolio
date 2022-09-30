abe645536872e1b945c18396a3af7dc8d24ac9b3
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-09-30 22:39:25 +0100

    adding intro animation

--- src/components/introSection.tsx
+++ src/components/introSection.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import developer_photo from "./../images/developer_photo.png";
 
 export const IntroSection = () => {
+  // const [tagLines, setTaglines] = useState([
+  //   "Hello, I'm Mazi Igwe!",
+  //   "I'm a frontend developer...",
+  // ]);
+  const [text, setText] = useState("");
+  const [typing, setTyping] = useState("Hello, I'm Mazi Igwe!");
+  const [letter, setLetter] = useState(0);
+
+  useEffect(() => {
+    if (letter <= typing.length) {
+      setTimeout(() => {
+        setText(text + typing[letter]);
+        setLetter(letter + 1);
+      }, 400);
+    } else {
+      setLetter(0);
+      setText("");
+    }
+  }, [letter]);
+
   return (
     <section id="home" className="intro-section ">
       <div className="container">
         <div className="row">
           <div className="col-md-6 intros">
             <h1 className="display-2">
-              <span className="display-2--intro">Hello, I'm Mazi Igwe!</span>
+              <span className="display-2--intro">{text}</span>
               <span className="display-2--description lh-base">
-                I'm a professional software developer. Experinced frontend web
-                developer.
+                A professional software developer. Experinced in frontend web
+                developement.
               </span>
             </h1>
             <button className="rounded-pill btn-rounded">
@@ -20,7 +41,15 @@ export const IntroSection = () => {
               </span>
             </button>
           </div>
-          <div className="col-md-6 intros"></div>
+          <div className="col-md-6 intros">
+            <div className="video-box text-center">
+              <img
+                className="img-fluid"
+                src={developer_photo}
+                alt="a developer image"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </section>
