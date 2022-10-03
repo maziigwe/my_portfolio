189ce365d29df9bf81e093c753600bc4dc552cc7
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-03 13:17:41 +0100

    started about-me section

---
+++ src/components/aboutSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about-me" className="about-me">
+      <div className="container">
+        <div className="row text-center">
+          <h4 className="fw-bolder mb-3">About me</h4>
+        </div>
+      </div>
+    </section>
+  );
+};
