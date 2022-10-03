aab1c8598ef238c2267d6e50cac6738f8d0ddb1c
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-03 18:28:04 +0100

    Added about section
      --about section started

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import "./App.css";
+import { AboutSection } from "./components/aboutSection";
 import { IntroSection } from "./components/introSection";
 import { Navbar } from "./components/navbar";
 
 function App() {
   return (
-    <main className="dark ">
+    <main className="light ">
       <Navbar />
       <IntroSection />
+      <AboutSection />
     </main>
   );
 }
-
 export default App;
--- src/components/aboutSection.tsx
+++ src/components/aboutSection.tsx
@@ -3,6 +3,14 @@ import React from "react";
 export const AboutSection = () => {
   return (
     <section id="about-me" className="about-me">
+      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150">
+        <path
+          // fill="#273036"
+          fill="#b1cefc"
+          fill-opacity="1"
+          d="M0,160L120,160C240,160,480,160,720,138.7C960,117,1200,75,1320,53.3L1440,32L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+        ></path>
+      </svg> */}
       <div className="container">
         <div className="row text-center">
           <h4 className="fw-bolder mb-3">About me</h4>
