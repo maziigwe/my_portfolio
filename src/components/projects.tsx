1df4ee47d0eb930148bbdca89cfdd96ee7fd4020
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-27 16:18:49 +0100

    added an open-source contribution project link

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -10,17 +10,19 @@ interface aboutProps {
 const projectList: project[] = [
   {
     id: 1,
-    title: "Project 1",
-    description: "Project 1 description",
-    repo: "",
+    title: "I4G-HACKTOBER OPEN-SOURCE CHALLENGE-2022",
+    description:
+      "TECHMART is an open-source project that I recently contributed to",
+    repo: "https://github.com/Ingressive-for-Good/I4G-OPENSOURCE-FRONTEND-PROJECT-2022",
     image:
-      "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+      "https://user-images.githubusercontent.com/70960505/193130210-583a1493-dc4d-4ca8-96cd-8cdf63daafd8.png",
   },
   {
     id: 2,
     title: "Project 2",
     description: "Description of project 2",
     repo: "",
+    isLive: true,
     image:
       "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
   },
