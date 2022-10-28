a4d7ac35518c80d165e7ed82f0389aed572134b1
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-28 07:09:58 +0100

    Modified project interface  --added type property

--- src/interfaces/project.tsx
+++ src/interfaces/project.tsx
@@ -1,8 +1,11 @@
+import { badgeTitle } from "./badge";
+
 export default interface project {
   id: Number;
   title: string;
   repo: string;
   description: string;
   image: string;
+  type: badgeTitle;
   isLive?: boolean | false;
 }
