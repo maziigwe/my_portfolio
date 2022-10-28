d04d53a2a17ae3043d1aacaf3bced30a2a02d0c9
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-10-28 07:16:24 +0100

    added the badge classes

--- src/models/project.tsx
+++ src/models/project.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import project from "../interfaces/project";
+import { Badge } from "../components/atoms/badge";
 
 export const Project = (item: project) => {
   return (
@@ -15,19 +16,23 @@ export const Project = (item: project) => {
         <p className="text-lowercase m-0 mb-1">
           {item.description + "" + item.description}
         </p>
-        <div className="text-right">
-          <a href={`${item.repo ? item.repo : ""}`}>
-            <div className=" btn btn-outline-primary mx-2  ">
-              {" "}
-              {"Code </>"}{" "}
+        <div className="flex justify-content-between">
+          <Badge title={item.type} />
+          <div className="text-right">
+            {" "}
+            <a href={`${item.repo ? item.repo : ""}`}>
+              <div className=" btn btn-outline-primary mx-2  ">
+                {" "}
+                {"Code </>"}{" "}
+              </div>
+            </a>
+            <div
+              className={`btn btn-primary mr-1  ${
+                !item.isLive ? " disabled " : ""
+              }`}
+            >
+              Live
             </div>
-          </a>
-          <div
-            className={`btn btn-primary mr-1  ${
-              !item.isLive ? " disabled " : ""
-            }`}
-          >
-            Live
           </div>
         </div>
       </div>
