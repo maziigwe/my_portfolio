ff458a8ab66da1583f27b8c26e1e04136d5eadc5
Igwe Acha Akwu <successoracha1995@gmail.com>
2022-11-13 00:08:13 +0100

    Added skills model

---
+++ src/models/skill.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import skill from "./../interfaces/skill";
+
+export const skill = (skillProps: skill) => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
